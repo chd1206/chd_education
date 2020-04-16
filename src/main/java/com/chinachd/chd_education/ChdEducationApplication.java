e759ee654180e4a0be024869b9eb034e29475fb9
chd <1551210563@qq.com>
2020-04-16 12:56:40 +0800

    创建我的教育网站

---
+++ src/main/java/com/chinachd/chd_education/ChdEducationApplication.java
@@ -0,0 +1,13 @@
+package com.chinachd.chd_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChdEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChdEducationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chinachd/chd_education/ChdEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinachd.chd_education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChdEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
